051ac0844c6eb5f89995381692f0195dd431387d
Davi Patricio <davipatricio2006@gmail.com>
2024-04-26 19:53:17 -0300

    fix: increase test timeout

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     exclude: ['**/node_modules', '**/dist', '.idea', '.git', '.cache'],
-    passWithNoTests: true
-  }
+    passWithNoTests: true,
+    testTimeout: 30000
+  },
 });
